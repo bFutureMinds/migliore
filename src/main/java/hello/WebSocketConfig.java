219c7796f477f671314961c4f12abc1c3ae54836
Shubham <shubhammuttepawar1@gmail.com>
2016-06-11 18:23:26 +0530

    adding cors to messahe handlers

--- src/main/java/hello/WebSocketConfig.java
+++ src/main/java/hello/WebSocketConfig.java
@@ -1,26 +1,30 @@
-package hello;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+/**
+ * Created by Shubham on 6/11/2016.
+ */
+    package hello;
 
-@Configuration
-@EnableWebSocket
-@EnableScheduling
-public class WebSocketConfig implements WebSocketConfigurer {
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.scheduling.annotation.EnableScheduling;
+    import org.springframework.web.socket.config.annotation.EnableWebSocket;
+    import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+    import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-    @Autowired
-    CounterHandler counterHandler;
-    @Autowired
-    OfferHandler offerHandler;
+    @Configuration
+    @EnableWebSocket
+    @EnableScheduling
+    public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(counterHandler, "/counter");
-        registry.addHandler(offerHandler, "/offers");
+        @Autowired
+        CounterHandler counterHandler;
+        @Autowired
+        OfferHandler offerHandler;
+
+        @Override
+        public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+            registry.addHandler(counterHandler, "/counter").setAllowedOrigins("*");
+            registry.addHandler(offerHandler, "/offers").setAllowedOrigins("*");
+        }
+
     }
-
-}

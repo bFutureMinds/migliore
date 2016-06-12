78d40e5acdba03be6e4d6467bc7a8501c737c930
Shubham <shubhammuttepawar1@gmail.com>
2016-06-12 06:22:06 +0530

    Adding missing getter setters

--- src/main/java/hello/controllers/RulesController.java
+++ src/main/java/hello/controllers/RulesController.java
@@ -12,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,23 +27,27 @@ public class RulesController {
     @Autowired
     RulesAdapter rulesAdapter;
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/getall", method = RequestMethod.GET)
     public ResponseEntity<List<TransactionalMasterStrategy>> getAllRules() throws JsonProcessingException {
         return new ResponseEntity<>(rulesAdapter.getAll(), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity addRule(@RequestBody TransactionalMasterStrategy strategy) {
         rulesAdapter.addRule(strategy);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/remove", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity removeRule(@RequestBody TransactionalMasterStrategy strategy) {
         rulesAdapter.deleteRule(strategy.getId());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/update", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity updateRule(@RequestBody TransactionalMasterStrategy strategy) {
         rulesAdapter.updateRule(strategy);
--- src/main/java/hello/rulesProcessor/models/TransactionTimeRule.java
+++ src/main/java/hello/rulesProcessor/models/TransactionTimeRule.java
@@ -13,6 +13,25 @@ public class TransactionTimeRule implements TransactionStrategy {
     private Time toTime;
     private Time fromTime;
 
+    public TransactionTimeRule() {
+    }
+
+    public Time getToTime() {
+        return toTime;
+    }
+
+    public void setToTime(Time toTime) {
+        this.toTime = toTime;
+    }
+
+    public Time getFromTime() {
+        return fromTime;
+    }
+
+    public void setFromTime(Time fromTime) {
+        this.fromTime = fromTime;
+    }
+
     public TransactionTimeRule(Time toTime, Time fromTime) {
         this.toTime = toTime;
         this.fromTime = fromTime;
